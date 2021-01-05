cd987580aa0981e22117c9e8ea3471ceab3e795a
Sara Pettigrew <sarajp.ak@hotmail.com>
2021-01-05 12:17:20 -0700

    More notes added to guided lecture code

--- src/queue-two-stacks_demo2.py
+++ src/queue-two-stacks_demo2.py
@@ -8,6 +8,7 @@ As you write your methods, you should optimize for time on the `enqueue()` and
 
 The Stack class that you will use has been provided to you.
 """
+# Utilize these stacks. One to hold all the data, the second to change FICO order. Pop from the first, push from the second
 class Stack:
     def __init__(self):
         self.data = []
@@ -23,31 +24,48 @@ class Stack:
 class QueueTwoStacks:
     def __init__(self):
         # Your code here
-        self.input_stack = Stack()
-        self.output_stack = Stack()
+        self.input_stack = Stack()      # left stack    
+        self.output_stack = Stack()     # right stack 
 
         
     def enqueue(self, item):
-        # Your code here
+        # Your code here - the original stack. Always add to the input stack. Here to fill if we need to add more to the output stack
         self.input_stack.push(item)
 
 
     def dequeue(self):
-        # Your code here
+        # Your code here - do not add new items to the ouput stack
         # return values from the output stack
-        # if the output stack is empty, fill it with values from the input stack
+        # if the output stack is empty, fill it with values from the input stack. 
         if len(self.output_stack.data) == 0:
+            # while there are items in the input stack
+            # check the length to make sure it's not greater than 0
             while len(self.input_stack.data) > 0:
                 # pop from the input-stack and push onto the the output-stack
                 cur_item = self.input_stack.pop()
+                # take each letter and move it over to the output stack, which is now in reverse (correct) order
                 self.output_stack.push(cur_item)
 
         # if there are items in the output stack, pop one off
         if len(self.output_stack.data) == 0:
+            # check if nothing is there, don't pop. Return None
             return None
         return self.output_stack.pop()
 
         self.input_stack.pop()
 
+        
+my_queue = QueueTwoStacks()
 
+my_queue.enqueue("A")
+my_queue.enqueue("B")
+my_queue.enqueue("C")
+my_queue.enqueue("D")
 
+print("The first item added was " + my_queue.dequeue())
+print("The second item added was " + my_queue.dequeue())
+
+my_queue.enqueue("E")
+print("The third item added was " + my_queue.dequeue())
+print("The fourth item added was " + my_queue.dequeue())
+print("The fifth item added was " + my_queue.dequeue())
--- src/stacks-objective.py
+++ src/stacks-objective.py
@@ -6,8 +6,23 @@ push - add to the top of the stack
 pop - remove from the top of the stack
 
 Very efficient to add or delete from the front (or the top)
-
 """
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def push(self, item):
+        # add to the end of the array or bottom
+        self.items.append(item)
+
+    def pop(self):
+        # always pops items off the back of the array
+        if len(self.items) == 0:
+            return None
+        last_item = self.items.pop()
+        return last_item
+
 
 class LinkedListNode:
     def __init__(self, data):
@@ -29,26 +44,34 @@ class LLStack:
     def pop(self):
         # don't remove anything from an empty stack
         if self.top is None:
+            # if stack is empty, return none
             return None
 
         old_top = self.top
+        #new top replaced old top
         self.top = old_top.next
-
+        # now old top is none because it's been replaced by a new top
         old_top.next = None 
 
         return old_top.data 
 
-class Stack:
-    def __init__(self):
-        self.top = None
+
+class ArrayStack:
+    def __init__I(self):
+        #initialize self.item to an empty array
+        self.item = []
 
     def push(self, item):
-        # add to the end of the array or bottom
+        # add items to the top of the stack in the array
         self.items.append(item)
 
     def pop(self):
-        # always pops items off the back of the array
+        # if there are no items, return None
         if len(self.items) == 0:
-            return None
+            return None  
+        # if there are items, remove the top from the stack      
         last_item = self.items.pop()
+        # return the last item in the array
         return last_item
+
+
